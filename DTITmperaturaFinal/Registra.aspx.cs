0252446cc18979f2642aa81c3efce21f4c2fc985
Allan <ferrerallan@gmail.com>
2019-09-01 10:10:46 -0300

    adiçao de exemplo gravacao em aspnet

---
+++ DTITmperaturaFinal/Registra.aspx.cs
@@ -0,0 +1,26 @@
+﻿using CALogin;
+using DAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TemperaturaDTI
+{
+    public partial class Registra : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            BOCALogin.setConexao("[famema.famema]");
+            string t = Request["t"].ToString();
+
+            string sql = "insert into sihosp.dti_temperatura( nro_temperatura ) values ("+t+") ";
+
+            string ret = BDOracle.executaComandoCommit(sql);
+            Response.Write(ret);
+            Response.Flush();
+        }
+    }
+}
